31268b1607b9ab5ad2cd6369daee42910415d918
Leandro Diaz <lainz@users.noreply.github.com>
2024-04-20 05:14:00 -0300

    background image

--- umain.pas
+++ umain.pas
@@ -6,7 +6,8 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs,
-  ustyles, BCPanel, BCMaterialEdit;
+  ustyles, BCPanel, BCMaterialEdit, BGRAVirtualScreen, BGRABitmap, BCTypes,
+  BGRABitmapTypes;
 
 type
 
@@ -16,9 +17,12 @@ type
     BCMaterialEdit1: TBCMaterialEdit;
     BCPanel1: TBCPanel;
     BCPanel2: TBCPanel;
+    BGRAVirtualScreen1: TBGRAVirtualScreen;
+    procedure BGRAVirtualScreen1Redraw(Sender: TObject; Bitmap: TBGRABitmap);
     procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
-
+    backgroundImage: TBGRABitmap;
   public
 
   end;
@@ -36,6 +40,18 @@ procedure TForm1.FormCreate(Sender: TObject);
 begin
   styleForm(Self);
   BCMaterialEdit1.Align := alTop;
+  backgroundImage := TBGRABitmap.Create;
+  backgroundImage.LoadFromResource('BACKGROUND');
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  backgroundImage.Free;
+end;
+
+procedure TForm1.BGRAVirtualScreen1Redraw(Sender: TObject; Bitmap: TBGRABitmap);
+begin
+  Bitmap.StretchPutImageProportionally(Rect(0, 0, Bitmap.Width, Bitmap.Height), taCenter, tlCenter, backgroundImage, dmSet, 255, True);
 end;
 
 end.
